56aa595f8d0acf005be26a28b93d5cb36b5429bc
Wenjie Xu <wenjie.xu.cn@outlook.com>
2025-04-01 22:06:58 +0800

    feat: :sparkles: Add logging and directory creation in config, and implement tool operations in utils

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
@@ -8,19 +8,36 @@ load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+# logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
+# Data paths
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
+# Models paths
 MODELS_DIR = PROJ_ROOT / "models"
 
+# Reports paths
 REPORTS_DIR = PROJ_ROOT / "reports"
+LOGS_DIR = REPORTS_DIR / "logs"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
+# Ensure all directories exist
+for directory in [
+    DATA_DIR,
+    RAW_DATA_DIR,
+    INTERIM_DATA_DIR,
+    PROCESSED_DATA_DIR,
+    EXTERNAL_DATA_DIR,
+    MODELS_DIR,
+    REPORTS_DIR,
+    LOGS_DIR,
+    FIGURES_DIR,
+]:
+    directory.mkdir(exist_ok=True, parents=True)
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
 try:
